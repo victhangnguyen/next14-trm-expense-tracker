6e34e149b8fd407373934f0c8a9c41e71d5433b9
victhangnguyen <victhangnguyen@outlook.com>
2024-09-04 00:33:29 +0700

    Create Guest comp in components, update Guest comp for page in root.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { currentUser } from "@clerk/nextjs/server";
+//! comps
+import Guest from "@/components/Guest";
 
-export default function Home() {
-  return <main>Expense Tracker</main>;
+export default async function Home() {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest />;
+  }
+
+  return (
+    <main>
+      <h1>Welcome, {user.firstName}</h1>
+    </main>
+  );
 }
 // <main className={styles.main}>
 // </main>
