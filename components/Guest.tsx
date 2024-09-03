8236603dc47f1d9c8ef4fe8bef640ae419aa9659
victhangnguyen <victhangnguyen@outlook.com>
2024-09-04 00:36:50 +0700

    Create Guest comp in components, update Guest comp for page in root.

---
+++ components/Guest.tsx
@@ -0,0 +1,15 @@
+import { SignInButton } from "@clerk/nextjs";
+
+interface GuestProps {}
+
+const Guest = (props: GuestProps) => {
+  return (
+    <div className="guest">
+      <h1>Welcome</h1>
+      <p>Please sign in to manage your transactions</p>
+      <SignInButton />
+    </div>
+  );
+};
+
+export default Guest;
